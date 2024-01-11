ead302413f6435bc4b690b0159608d469bb40957
Liliane-Top <hallo@lilianetop.nl>
2024-01-11 16:28:45 +0100

    step 4 adding RegisteredClientRepository for authorization client flow

--- src/main/java/nl/lilianetop/spring6authserver/config/SecurityConfig.java
+++ src/main/java/nl/lilianetop/spring6authserver/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package nl.lilianetop.spring6authserver.config;
 
+import java.util.UUID;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -9,8 +10,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -55,7 +63,7 @@ public class SecurityConfig {
 
     return http.build();
   }
-
+//will not be used for the client authorization flow
   @Bean
   public UserDetailsService userDetailsService() {
     UserDetails userDetails = User.withDefaultPasswordEncoder()
@@ -65,6 +73,26 @@ public class SecurityConfig {
         .build();
 
     return new InMemoryUserDetailsManager(userDetails);
+  }
+
+  @Bean
+  public RegisteredClientRepository registeredClientRepository() {
+    RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+        .clientId("oidc-client")
+        .clientSecret("{noop}secret")//usually you encrypt this secret and stored safe
+        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+        .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+        .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+        .postLogoutRedirectUri("http://127.0.0.1:8080/")
+        .scope(OidcScopes.OPENID)
+        .scope(OidcScopes.PROFILE)
+        .scope("message.read")
+        .scope("message.write")
+        .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+        .build();
+
+    return new InMemoryRegisteredClientRepository(oidcClient);
   }
 
 }
