4eac79bae5606cb714d0d0bb9b979c0f01758581
LilianeTop <hallo@lilianetop.nl>
2024-01-11 15:36:46 +0100

    Initial commit

---
+++ src/main/java/nl/lilianetop/spring6authserver/Spring6AuthserverApplication.java
@@ -0,0 +1,13 @@
+package nl.lilianetop.spring6authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6AuthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6AuthserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/lilianetop/spring6authserver/Spring6AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.lilianetop.spring6authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
